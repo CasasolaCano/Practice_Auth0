db29745187378d6428576ca64c42602ff5e165f6
CasasolaCano <casasolacontact@gmail.com>
2021-03-25 13:28:07 +0100

    block service with ng guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PricesComponent } from './components/prices/prices.component';
 import { ProtectComponent } from './components/protect/protect.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path: 'prices', component: PricesComponent},
-  {path: 'protect', component: ProtectComponent},
+  {
+    path: 'protect', component: ProtectComponent,
+    canActivate: [AuthGuard]
+  },
   {path: "**", pathMatch: 'full', component: HomeComponent}
 ];
 
